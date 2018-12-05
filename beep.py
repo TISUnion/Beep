5301d3d0381097d90cc6026dabc20175b3440d09
LucunJi <36262513+LucunJi@users.noreply.github.com>
2018-12-05 13:49:37 +0800

    Update beep.py

--- beep.py
+++ beep.py
@@ -6,7 +6,7 @@ import traceback
 def onServerInfo(server, info):
   if info.isPlayer == 1:
     if info.content.find('@ ') > -1:
-      nameList = re.findall('(?<=@ )\S+(?= )',info.content)
+      nameList = re.findall('(?<=@ )\S+',info.content)
       if nameList:
         for name in nameList:
           server.execute('execute at ' + name + ' run playsound minecraft:entity.arrow.hit_player player ' + name)
