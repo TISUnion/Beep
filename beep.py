2adb5197baa084fed96119891d7e250e6ef43886
LucunJi <36262513+LucunJi@users.noreply.github.com>
2018-12-05 12:33:52 +0800

    Update beep.py

--- beep.py
+++ beep.py
@@ -1,9 +1,16 @@
+# -*- coding: utf-8 -*-
+
 import re
+import traceback
 
 def onServerInfo(server, info):
-    if info.isPlayer == 1:
-        server.say('start finding name')
-        nameList = re.findall('(?<=@)\S+(?= )',info.content)
-        for name in nameList:
-            server.say(name)
-            server.execute('playsound minecraft:entity.arrow.hit_player player ' + name)
+    try:
+        if info.isPlayer == 1:
+            if info.content.find('@') > -1:
+                nameList = re.findall('(?<=@)\S+(?= )',info.content)
+                for name in nameList:
+                    server.execute('playsound minecraft:entity.arrow.hit_player player ' + name)
+    except:
+        lines = traceback.format_exc().splitlines()
+        for l in lines:
+            server.say(l)
