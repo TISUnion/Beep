7e3df4ea77ad3ee24536307e196467f4bef5651e
LucunJi <36262513+LucunJi@users.noreply.github.com>
2018-12-05 12:35:48 +0800

    Update beep.py

--- beep.py
+++ beep.py
@@ -5,9 +5,13 @@ import traceback
 
 def onServerInfo(server, info):
     try:
+        server.say('被调用')
         if info.isPlayer == 1:
+            server.say('判断是否存在@符号')
             if info.content.find('@') > -1:
+                server.say('开始调用正则表达式')
                 nameList = re.findall('(?<=@)\S+(?= )',info.content)
+                server.say(str(nameList))
                 for name in nameList:
                     server.execute('playsound minecraft:entity.arrow.hit_player player ' + name)
     except:
