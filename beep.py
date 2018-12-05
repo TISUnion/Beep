a15e4cb82c4ec26d729f255e2e03329486616d16
LucunJi <36262513+LucunJi@users.noreply.github.com>
2018-12-05 12:57:22 +0800

    Update beep.py

--- beep.py
+++ beep.py
@@ -5,10 +5,8 @@ import traceback
 
 def onServerInfo(server, info):
   if info.isPlayer == 1:
+    server.say(info.content)
     if info.content.find('@') > -1:
       nameList = re.findall('(?<=@)\S+',info.content)
-      server.say(str(nameList))
       for name in nameList:
-        server.execute('gamerule sendCommandFeedback false')
         server.execute('execute at ' + name + ' run playsound minecraft:entity.arrow.hit_player player ' + name)
-        server.execute('gamerule sendCommandFeedback true')
