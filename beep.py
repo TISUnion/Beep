0400c15e236db8ee5a7e10a7ca2b4359ed16b362
LucunJi <36262513+LucunJi@users.noreply.github.com>
2018-12-05 12:30:11 +0800

    Update beep.py

--- beep.py
+++ beep.py
@@ -4,4 +4,4 @@ def onServerInfo(server, info):
     if info.isPlayer == 1:
         nameList = re.findall('(?<=@)\S+(?= )',info.content)
         for name in nameList:
-            server.execute('playsound random.orb player ' + name)
+            server.execute('playsound minecraft:entity.arrow.hit_player player ' + name)
