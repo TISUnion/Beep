984caef330986c86a0ee424907356d988e8c1802
Fallen_Breath <yqx791125833@gmail.com>
2020-04-09 11:58:48 +0800

    MCDR compatibility

--- beep.py
+++ beep.py
@@ -2,6 +2,7 @@
 
 import re
 import traceback
+import copy
 
 def onServerInfo(server, info):
   if info.isPlayer == 1:
@@ -13,3 +14,8 @@ def onServerInfo(server, info):
             server.execute('execute at @a run playsound minecraft:entity.arrow.hit_player player @a')
           else:
             server.execute('execute at ' + name + ' run playsound minecraft:entity.arrow.hit_player player ' + name)
+
+def on_info(server, info):
+  info2 = copy.deepcopy(info)
+  info2.isPlayer = info2.is_player
+  onServerInfo(server, info2)
