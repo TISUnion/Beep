113db6fbd41e6f7e3246676244702497bd93a104
Fallen_Breath <yqx791125833@gmail.com>
2020-05-11 22:33:18 +0800

    use @@ to title the one you want to @

--- beep.py
+++ beep.py
@@ -1,21 +1,56 @@
 # -*- coding: utf-8 -*-
-
-import re
-import traceback
 import copy
+import json
+import re
+import time
+
+
+def beep_small(server, info, name_list):
+	for name in name_list:
+		server.execute('execute at {0} run playsound minecraft:entity.arrow.hit_player player {0}'.format(name))
+
+
+def beep_big(server, info, name_list):
+	if not name_list:
+		return
+	source = info.player if info.isPlayer else '控制台'
+	text = json.dumps([
+		{
+			'text': source,
+			'color': 'aqua'
+		},
+		{
+			'text': '在@你',
+			'color': 'white'
+		}
+	])
+	for name in name_list:
+		server.execute('title {} times 2 15 5'.format(name))
+		server.execute('title {} title {}'.format(name, text))
+	for i in range(3):
+		beep_small(server, info, name_list)
+		time.sleep(1 / 3)
+
+
+def find_name_list(text, pattern):
+	name_list = None
+	if text.find('{} '.format(pattern)) > -1:
+		name_list = re.findall(r'(?<={} )\S+'.format(pattern), text)
+	if not name_list:
+		name_list = []
+	for i, name in enumerate(name_list):
+		if name == 'all':
+			name_list[i] = '@a'
+	return name_list
+
 
 def onServerInfo(server, info):
-  if info.isPlayer == 1:
-    if info.content.find('@ ') > -1:
-      nameList = re.findall('(?<=@ )\S+',info.content)
-      if nameList:
-        for name in nameList:
-          if name == 'all':
-            server.execute('execute at @a run playsound minecraft:entity.arrow.hit_player player @a')
-          else:
-            server.execute('execute at ' + name + ' run playsound minecraft:entity.arrow.hit_player player ' + name)
+	if info.isPlayer == 1 or (hasattr(server, 'MCDR') and info.is_user) and info.content.find('@ ') > -1:
+		beep_small(server, info, find_name_list(info.content, '@'))
+		beep_big(server, info, find_name_list(info.content, '@@'))
+
 
 def on_info(server, info):
-  info2 = copy.deepcopy(info)
-  info2.isPlayer = info2.is_player
-  onServerInfo(server, info2)
+	info2 = copy.deepcopy(info)
+	info2.isPlayer = info2.is_player
+	onServerInfo(server, info2)
