cadfffeb8b5122bd84f4ba7ae3a5490d7d2fde30
Fallen_Breath <yqx791125833@gmail.com>
2021-01-15 00:04:21 +0800

    MCDR v1.0 support and MCDR v1.0+ only

--- beep.py
+++ beep.py
@@ -1,19 +1,29 @@
-# -*- coding: utf-8 -*-
-import copy
 import json
 import re
 import time
 
+from mcdreforged.api.decorator import new_thread
+from mcdreforged.api.types import *
+
+PLUGIN_METADATA = {
+	'id': 'beep',
+	'version': '1.0.0'
+}
+
 
 def beep_small(server, info, name_list):
+	if not name_list:
+		return
+	info.cancel_send_to_server()
 	for name in name_list:
 		server.execute('execute at {0} run playsound minecraft:entity.arrow.hit_player player {0}'.format(name))
 
 
-def beep_big(server, info, name_list):
+def beep_big(server: ServerInterface, info: Info, name_list):
 	if not name_list:
 		return
-	source = info.player if info.isPlayer else '控制台'
+	info.cancel_send_to_server()
+	source = info.player if info.is_player else '控制台'
 	text = json.dumps([
 		{
 			'text': source,
@@ -24,12 +34,17 @@ def beep_big(server, info, name_list):
 			'color': 'white'
 		}
 	])
-	for name in name_list:
-		server.execute('title {} times 2 15 5'.format(name))
-		server.execute('title {} title {}'.format(name, text))
-	for i in range(2):
-		time.sleep(1.0 / 3)
-		beep_small(server, info, name_list)
+
+	@new_thread('beep')
+	def beeeeeeep():
+		for name in name_list:
+			server.execute('title {} times 2 15 5'.format(name))
+			server.execute('title {} title {}'.format(name, text))
+		for i in range(2):
+			time.sleep(1.0 / 3)
+			beep_small(server, info, name_list)
+
+	beeeeeeep()
 
 
 def find_name_list(text, pattern):
@@ -44,17 +59,11 @@ def find_name_list(text, pattern):
 	return name_list
 
 
-def onServerInfo(server, info):
-	if info.isPlayer == 1 or (hasattr(server, 'MCDR') and info.is_user) and info.content.find('@ ') > -1:
+def on_user_info(server: ServerInterface, info: Info):
+	if info.is_user and info.content.find('@ ') > -1:
 		beep_small(server, info, find_name_list(info.content, '@'))
 		beep_big(server, info, find_name_list(info.content, '@@'))
 
 
-def on_info(server, info):
-	info2 = copy.deepcopy(info)
-	info2.isPlayer = info2.is_player
-	onServerInfo(server, info2)
-
-
-def on_load(server, old):
-	server.add_help_message('@ xxx', '@某人，@ all可作用于所有玩家。使用两个@@效果更佳')
+def on_load(server: ServerInterface, old):
+	server.register_help_message('@ xxx', '@某人，@ all可作用于所有玩家。使用两个@@小心被打')
