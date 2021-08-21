779646cf038e498af9ed9f3c5ac7ef613a851713
Fallen_Breath <yqx791125833@gmail.com>
2021-08-21 23:29:03 +0800

    mcdr2.0

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Beep
+
 Beeps when someone is mentioned in text with an `@`
 
 Leave a space before and after name when you use a @, shuch as `@ Lucun_Ji Hello!`
 
 `@ all ` represents all players in game
 
-use two `@` for more powerful beep
+use two `@` for more powerful ping
--- beep/__init__.py
+++ beep/__init__.py
@@ -1,14 +1,7 @@
-import json
 import re
 import time
 
-from mcdreforged.api.decorator import new_thread
-from mcdreforged.api.types import *
-
-PLUGIN_METADATA = {
-	'id': 'beep',
-	'version': '1.0.0'
-}
+from mcdreforged.api.all import *
 
 
 def beep_small(server, info, name_list):
@@ -23,23 +16,16 @@ def beep_big(server: ServerInterface, info: Info, name_list):
 	if not name_list:
 		return
 	info.cancel_send_to_server()
-	source = info.player if info.is_player else '控制台'
-	text = json.dumps([
-		{
-			'text': source,
-			'color': 'aqua'
-		},
-		{
-			'text': '在@你',
-			'color': 'white'
-		}
-	])
+	source_name = RText(info.player if info.is_player else server.tr('beep.console'), RColor.aqua)
+	title: RTextBase = server.tr('beep.title', source_name)
+	subtitle: RTextBase = server.tr('beep.subtitle', source_name)
 
 	@new_thread('beep')
 	def beeeeeeep():
 		for name in name_list:
 			server.execute('title {} times 2 15 5'.format(name))
-			server.execute('title {} title {}'.format(name, text))
+			server.execute('title {} title {}'.format(name, title.to_json_str()))
+			server.execute('title {} subtitle {}'.format(name, subtitle.to_json_str()))
 		for i in range(2):
 			time.sleep(1.0 / 3)
 			beep_small(server, info, name_list)
@@ -60,10 +46,10 @@ def find_name_list(text, pattern):
 
 
 def on_user_info(server: ServerInterface, info: Info):
-	if info.is_user and info.content.find('@ ') > -1:
+	if info.content.find('@ ') > -1:
 		beep_small(server, info, find_name_list(info.content, '@'))
 		beep_big(server, info, find_name_list(info.content, '@@'))
 
 
-def on_load(server: ServerInterface, old):
-	server.register_help_message('@ xxx', '@某人，@ all可作用于所有玩家。使用两个@@小心被打')
+def on_load(server: PluginServerInterface, old):
+	server.register_help_message('@ <someone>', server.get_self_metadata().description)
